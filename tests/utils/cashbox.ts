331e69f1abdd163fdfceb15c5643ab26ae1dabb0
Tom Peres <perestom@gmail.com>
2022-07-27 20:25:51 +0300

    fixed prettier

--- tests/utils/cashbox.ts
+++ tests/utils/cashbox.ts
@@ -1,8 +1,8 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Keypair, PublicKey } from '@solana/web3.js';
-import { KachingCashRegister } from '../../target/types/kaching_cash_register';
-import { getConnection } from './solana';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { KachingCashRegister } from "../../target/types/kaching_cash_register";
+import { getConnection } from "./solana";
 
 const program = anchor.workspace
   .KachingCashRegister as Program<KachingCashRegister>;
@@ -10,16 +10,16 @@ const program = anchor.workspace
 const randomLowerCaseCharCode = () =>
   [1, 2, 3]
     .map(() => String.fromCharCode(97 + Math.floor(Math.random() * 25)))
-    .join('');
+    .join("");
 
-export const generateRandomCashboxId = (prefix: string = 'my_test_cashbox_') =>
+export const generateRandomCashboxId = (prefix: string = "my_test_cashbox_") =>
   `${prefix}${randomLowerCaseCharCode()}`;
 
 export const findCashboxPDA = async (cashboxId: string) =>
   PublicKey.findProgramAddress(
     [
-      anchor.utils.bytes.utf8.encode('cashbox'),
-      Buffer.from(cashboxId, 'ascii'),
+      anchor.utils.bytes.utf8.encode("cashbox"),
+      Buffer.from(cashboxId, "ascii"),
     ],
     program.programId
   );
@@ -48,6 +48,6 @@ export const createCashbox = async (
 
   if (options.waitForTx) {
     const connection = getConnection();
-    await connection.confirmTransaction(tx, 'finalized');
+    await connection.confirmTransaction(tx, "finalized");
   }
 };
